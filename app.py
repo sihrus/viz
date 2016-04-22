337525cae222e332b71bd10ac62e6346738a1a82
Mark Fontana <markfontana@Marks-MacBook-Air.local>
2016-04-22 16:20:17 -0400

    no saving data locally anymore

--- app.py
+++ app.py
@@ -9,6 +9,32 @@ app.vars={}
 @app.route('/')
 def main():
   return redirect('/index')
+  #return redirect('/indexd3')
+  #return redirect('/d3buses')
+
+'''
+# used for testing d3... ended up not using!
+@app.route('/d3buses', methods=['GET', 'POST'])
+def d3buses():
+
+  MTA_API_BASE = 'http://bustime.mta.info/api/siri/vehicle-monitoring.json?key={key}'
+  MTA_API_KEY = 'ffac85e6-bf25-4762-9b43-d46b5ad2f5d8'
+  # example: https://github.com/conorbranagan/bustime/blob/master/bus_status.py
+  # documentation: http://bustime.mta.info/wiki/Developers/SIRIVehicleMonitoring
+  df = nyc_current(MTA_API_BASE,MTA_API_KEY)
+  
+  df_lat_lon = df[['MonitoredVehicleJourney_VehicleLocation_Latitude','MonitoredVehicleJourney_VehicleLocation_Longitude']]
+  jsonDf = df_lat_lon.dropna().to_json(orient='records')
+  output = jsonDf.replace('{\"MonitoredVehicleJourney_VehicleLocation_Latitude\":',"[")
+  output = output.replace(',\"MonitoredVehicleJourney_VehicleLocation_Longitude\":',",")
+  output = output.replace('}',"]")
+
+  return render_template('d3buses.html', output=output)
+
+@app.route('/indexd3', methods=['GET', 'POST'])
+def indexd3():
+  return render_template('indexd3.html')
+'''
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
@@ -54,19 +80,16 @@ def buses():
   output = jsonDf.replace('{\"MonitoredVehicleJourney_VehicleLocation_Latitude\":',"[")
   output = output.replace(',\"MonitoredVehicleJourney_VehicleLocation_Longitude\":',",")
   output = output.replace('}',"]")
-  output = "var buses = " + output + ";"
-  text_file = open("static/data/busesnow.js", "w")
-  text_file.write(output)
-  text_file.close()
+
   df['total'] = 1
   grouped = df.groupby(['MonitoredVehicleJourney_LineRef','MonitoredVehicleJourney_DestinationName'])
   df_count = grouped.count().reset_index()[['MonitoredVehicleJourney_LineRef','MonitoredVehicleJourney_DestinationName','total']]
   df_count.rename(columns={'MonitoredVehicleJourney_LineRef': 'Line'}, inplace=True)
   df_count.rename(columns={'MonitoredVehicleJourney_DestinationName': 'Direction'}, inplace=True)
   df_count.rename(columns={'total': 'Total Buses On Route'}, inplace=True)
-  
-  output = df_count.to_html(index=False)
-  return render_template('buses.html', output=output)
+  output2 = df_count.to_html(index=False)
+
+  return render_template('buses.html', output=output, output2=output2)
 
 @app.route('/mybuses', methods=['GET','POST'])
 def mybuses():
@@ -85,20 +108,18 @@ def mybuses():
     output = jsonDf.replace('{\"MonitoredVehicleJourney_VehicleLocation_Latitude\":',"[")
     output = output.replace(',\"MonitoredVehicleJourney_VehicleLocation_Longitude\":',",")
     output = output.replace('}',"]")
-    output = "var buses = " + output + ";"
-    text_file = open("static/data/mybusesnow.js", "w")
-    text_file.write(output)
-    text_file.close()
+
     df['total'] = 1
     grouped = df.groupby(['MonitoredVehicleJourney_LineRef','MonitoredVehicleJourney_DestinationName'])
     df_count = grouped.count().reset_index()[['MonitoredVehicleJourney_LineRef','MonitoredVehicleJourney_DestinationName','total']]
     df_count.rename(columns={'MonitoredVehicleJourney_LineRef': 'Line'}, inplace=True)
     df_count.rename(columns={'MonitoredVehicleJourney_DestinationName': 'Direction'}, inplace=True)
     df_count.rename(columns={'total': 'Total Buses On Route'}, inplace=True)
-    
-    output = df_count.to_html(index=False)
-    return render_template('mybuses.html', output=output)
+    output2 = df_count.to_html(index=False)
+
+    return render_template('mybuses.html', output=output, output2=output2)
   except:
+    flash('INVALID BUS CODE!')
     print "ERROR, BAD MTA CODE"
 
 
